30d76861a3645a2f4e663194d806354e49e42160
Kimjinmo <jinmo@greff.co.jp>
2025-06-03 15:38:42 +0900

    fix: add webpack alias

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
+  webpack: (config) => {
+    config.resolve.alias["@" ] = require("path").resolve(__dirname, "src");
+    return config;
+  },
 };
 
 export default nextConfig;
