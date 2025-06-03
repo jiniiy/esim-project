d03844e4e903ecbba8a95300c7898b421563f316
Kimjinmo <jinmo@greff.co.jp>
2025-06-03 15:44:31 +0900

    fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
-/** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
   experimental: {
     optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
   },
   webpack: (config) => {
-    config.resolve.alias["@" ] = require("path").resolve(__dirname, "src");
+    config.resolve.alias["@" ] = path.resolve(__dirname, "src");
     return config;
   },
 };
