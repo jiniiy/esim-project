fb9e451144af63fb7c9e0a974f86172eb530e806
Kimjinmo <jinmo@greff.co.jp>
2025-06-04 23:06:23 +0900

    feat: redirect alert

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { Button, Container, SimpleGrid } from "@mantine/core";
 import { useAuth } from "@/context/AuthContext";
 import { COUNTRIES } from "@/constants";
+import { useEffect } from "react";
 
 export default function HomePage() {
   const { user } = useAuth();
   const router = useRouter();
-
+  const searchParams = useSearchParams();
   const handleCountryClick = (country: string) => {
     if (!user) {
       alert("You need log-in");
@@ -17,6 +18,14 @@ export default function HomePage() {
 
     router.push(`/country/${country}`);
   };
+
+  useEffect(() => {
+    const redirectFrom = searchParams.get("redirect");
+    if (redirectFrom) {
+      router.replace("/");
+      alert(`login required to access ${redirectFrom}`);
+    }
+  }, []);
 
   return (
     <Container className="p-10">
